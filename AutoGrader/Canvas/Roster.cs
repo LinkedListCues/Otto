af4fe78e4063565fa394f269d0ed147111c2d2c2
Ethan Robison <ethancrobison@gmail.com>
2018-05-16 16:29:44 -0500

    download all submissions now; store them all

--- AutoGrader/Canvas/Roster.cs
+++ AutoGrader/Canvas/Roster.cs
@@ -1,15 +1,12 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Net;
+using AutoGrader.Canvas;
 
 namespace AutoGrader
 {
     public class Roster : IManager
     {
         private const string INFO_PATH = "submissions.json";
-
-        private string _tempurl =
-            @"https://canvas.northwestern.edu/api/v1/courses/72859/assignments/458956/submissions?access_token=1876~nSmP6pGTi0LsIdPe8h19TLVL9zAP5tHTgvfMd08cjLZAdarU0HF5KQSyss8JGcdp&per_page=99&page=1";
 
         public List<Submission> Submissions { get; private set; }
 
@@ -25,37 +22,29 @@ namespace AutoGrader
         private void LoadRoster () {
             Logger.Log("Loading serialized roster...");
             Submissions = Serializer.ReadObjectFromPath<List<Submission>>(INFO_PATH);
-            foreach (var submission in Submissions) {
-                Logger.Log(submission.SubmissionID);
-            }
         }
 
         // todo make sure to get all 200 (only doing 99 atm)
         private void FetchAndSaveRoster () {
             Logger.Log("Downloading all submissions...");
             Submissions = new List<Submission>();
-            var json = LoadJsonArrayFromURL(_tempurl);
-            foreach (var child in json.Children<JObject>()) {
-                Submissions.Add(new Submission(child));
+
+            JArray json;
+            for (int i = 1; i < 99; i++) { // TODO this is a magic constant and needs to be adjusted
+                json = Config.Fetcher.FetchSubmissions(i);
+
+                foreach (var child in json.Children<JObject>()) {
+                    Submissions.Add(new Submission(child));
+                }
+
+                // we're getting 99 submissions per page, so if we have fewer, it's time to bail
+                if (json.Count < 99) { break; } // todo this is a slightly different magic constant
+                
             }
+
+
             Logger.Log("Serializing submissions...");
             Serializer.WriteObjectToPath(Submissions, INFO_PATH);
-        }
-
-        // todo error catching
-        private static JArray LoadJsonArrayFromURL (string url) {
-            using (var webClient = new WebClient()) {
-                string json = webClient.DownloadString(url);
-                return JArray.Parse(json);
-            }
-        }
-
-        // todo error catching
-        private static JObject LoadJsonFromURL (string url) {
-            using (var webClient = new WebClient()) {
-                string json = webClient.DownloadString(url);
-                return JObject.Parse(json);
-            }
         }
     }
 }
