3040f39b33540f67ff17ca4b021e82ee0ea594aa
Ethan Robison <ethancrobison@gmail.com>
2018-05-30 12:36:59 -0500

    simplify, my dude

--- AutoGrader/Utils/Logger.cs
+++ AutoGrader/Utils/Logger.cs
@@ -4,12 +4,6 @@ namespace AutoGrader.Utils
 {
     public static class Logger
     {
-        public static void Log(string message) {
-            Console.WriteLine(message);
-        }
-
-        public static void Log(object obj) {
-            Console.WriteLine(obj.ToString());
-        }
+        public static void Log (object message) { Console.WriteLine(message.ToString()); }
     }
 }
